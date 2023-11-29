a15e9a5dcd8a88d671e30ee3bb8df99f405df777
natenatep420 <natenatep20@gmail.com>
2023-11-28 23:11:41 -0600

    made changes to main.tf

--- main copy.tf
+++ main copy.tf
@@ -11,9 +11,5 @@ resource "kubernetes_namespace" "example" {
     name = var.name
   }
 }
-
 variable name {
-  description = "Please provide a name for the namespace"
-  type = string
-  default = ""
-}
+  
