88cd33b710cd6bbe219cdac2787565aa096fbee3
natenatep420 <natenatep20@gmail.com>
2023-11-28 23:23:55 -0600

    added input from user

--- main.tf
+++ main.tf
@@ -11,9 +11,3 @@ resource "kubernetes_namespace" "example" {
     name = var.name
   }
 }
-
-variable name {
-  description = "Please provide a name for the namespace"
-  type = string
-  default = ""
-}
